4ad3f3fb24315cac0f8381237909328ee15943c3
ashish-greycube <mr.ashish.shah@gmail.com>
2022-04-08 06:36:47 +0200

    minor: doctypes created

---
+++ oms/oms/doctype/fulfilment_center_assignment_rule/fulfilment_center_assignment_rule.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, GreyCube Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Fulfilment Center Assignment Rule', {
+	// refresh: function(frm) {
+
+	// }
+});
