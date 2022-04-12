e31a864e17ff27c19dc470929b111fd37184e2ec
ashish-greycube <mr.ashish.shah@gmail.com>
2022-04-12 14:00:45 +0200

    report structure

---
+++ oms/oms/report/order_fulfilment/order_fulfilment.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, GreyCube Technologies and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Order Fulfilment"] = {
+	"filters": [
+
+	]
+};
---
+++ oms/oms/report/order_fulfilment/order_fulfilment.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, GreyCube Technologies and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
