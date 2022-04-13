d7d1b3716109d0c7f08a06d57c760e6908c7c0e5
ashish-greycube <mr.ashish.shah@gmail.com>
2022-04-13 13:16:20 +0200

    minor: multi select field name

--- oms/oms/doctype/fulfilment_center_model/fulfilment_center_model.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, GreyCube Technologies and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class FulfilmentCenterModel(Document):
-	pass
