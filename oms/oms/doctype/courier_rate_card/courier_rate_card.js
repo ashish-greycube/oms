129310d6a8fd97b4505264ac0f3c4b8c3bbb0dd3
ashish-greycube <mr.ashish.shah@gmail.com>
2022-05-27 14:30:34 +0200

    minor: json changes for courier

---
+++ oms/oms/doctype/courier_rate_card/courier_rate_card.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, GreyCube Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Courier Rate Card', {
+	// refresh: function(frm) {
+
+	// }
+});
